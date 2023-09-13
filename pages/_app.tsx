0f639fa22172dc6761c7c0e1f3adea30ab02924e
Isa yj <isayjnai@outlook.com>
2023-09-13 20:18:58 +0200

    feat: create modal template and the Login modal based on it
    Modal template includes a header(with title and the close button), a footer button and a faded background.
    Login modal includes placeholders and closing function only.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,11 @@ import type { AppProps } from 'next/app'
 
 import Layout from '@/components/Layout'
 import '@/styles/globals.css'
-import Modal from '@/components/Modal'
+import LoginModal from '@/components/modals/LoginModal'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Modal />
+      <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
