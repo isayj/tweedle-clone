e8c61452c99aa1a0426a3ca8638f3de11f5ee8e4
Isa yj <isayjnai@outlook.com>
2023-09-27 13:55:32 +0200

    fix: button width
    button width: was blocked by wrong placement of the comments

--- components/Button.tsx
+++ components/Button.tsx
@@ -29,9 +29,7 @@ const Button: React.FC<ButtonProps> = ({
                 hover:opacity-80
                 transition
                 border-2
-                // Conditions
-                // If have width, use w-full
-                // Otherwise, use w-fit
+
                 ${fullWidth ? 'w-full' : 'w-fit'}
                 ${secondary ? 'bg-white' : 'bg-sky-500'}
                 ${secondary ? 'text-black' : 'text-white'}
